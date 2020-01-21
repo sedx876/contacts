661b777660d6c0b8855e73d135110a47f3ccde0c
sedx876 <swatkins876@gmail.com>
2020-01-21 16:30:43 -0500

    people controller and person model work

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,10 +1,6 @@
 class PeopleController < ApplicationController
 def index 
-    render json: [
-        { name: 'Darth Vader', age: 45},
-        { name: 'Sarah', age: 42},
-        { name: 'Cthulhu', age: 8000}
-    ]
+    render json: Person.all
 end
 
 
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,13 @@
 class Person < ApplicationRecord
+
+def self.all
+    [
+        { name: 'Darth Vader', age: 45},
+        { name: 'Sarah', age: 42},
+        { name: 'Cthulhu', age: 8000}
+    ]
+end
+
+
+
 end
